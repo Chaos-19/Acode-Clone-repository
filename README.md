f8123f0179d706e6a88e41b78d2ab1794025b7da
Chaos-19 <kalgetachew375@gmail.com>
2023-10-29 14:17:30 +0300

    update dir chooser

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 ## Features
 
 - Clone Git repositories to your local machine
-- Support for authentication for cloning private repositories
+- Support for authentication for cloning private repositories you need  username and token to clone private repository
 - Select the directory to clone the repository to from the available directories in your workspace
 
 ## Installation
@@ -19,21 +19,7 @@ To install the plugin, open Acode and go to Extensions > Manage Extensions. Sear
 1. To clone a Git repository, open Acode and go to File > Clone Git Repository.
 2. Enter the URL of the repository you want to clone and click ok.
 3. If the repository is private, you will be prompted to enter your credentials.
-4. Once the repository has been cloned, it will appear in the Workspace pane. You can then open the repository by double-clicking on it.
-
-## Selecting the Clone Directory
-
-To select the directory to clone the repository to, click the Select Directory button in the Clone Git Repository dialog box. A file picker will appear, allowing you to select the desired directory.
-
-## Troubleshooting
-
-If you are having problems cloning a repository, please check the following:
-
-- Make sure that you have entered the correct URL for the repository.
-- If the repository is private, make sure that you have entered the correct credentials.
-- Ensure that you have enough disk space to clone the repository.
-
-If you are still having problems, please create an issue on my plugin's GitHub repository.
+4. Once the repository has been cloned , a Workspace picker will appear, allowing you to select the desired directory the ,the repository will appear in the Workspace pane you just seleted. You can then open the repository by double-clicking on it.
 
 ## Contributions and Acknowledgments
 
@@ -43,10 +29,8 @@ I would like to express my gratitude to the open-source communities behind [Ligh
 
 If you have any feedback on the plugin, please feel free to leave a comment below or create an issue on my plugin's GitHub repository.
 [CLONE REPOSITORY](https://github.com/Chaos-19/Acode-Clone-repository.git)
-## Drawback
 
-Currently, the plugin will block other operations because it doesn't use a web worker. This will be fixed in the next update.
 
 ## License
 
-[License Information Here]
+[]
--- src/main.js
+++ src/main.js
@@ -68,325 +68,321 @@ class AcodePlugin {
     }
   }
 
+  async getDir() {
+    const fileBrowser = acode.require('fileBrowser');
+    const myFile = await fileBrowser('folder', 'Open a sample file', true);
+    return myFile;
+  }
+
+
   async getDirToSaveTheFile() {
-    const select = acode.require('select');
+    const fileBrowser = acode.require('fileBrowser');
+    const folder = await fileBrowser('folder', 'SELECT DIRECTORY', true);
+    let selectedPath = folder.url
+    const options = [folder.url,
+      folder.name
+    ];
 
-    const directories = window.addedFolder.map(folder => {
-      return [
-        folder.url,
-        folder.title,
-        "icon folder", true
-      ]
-    })
-
-    const options = directories;
-    const opt = {
-      onCancel: () => window.toast('Cancel Clicked', 3000),
-      hideOnSelect: true,
-      textTransform: true,
-      default: '',
-      };
-      let selectedPath = await select('SELECT DIRECTORY', options, opt);
-      const check = selectedPath.includes("::primary:");
-      if (!check && !selectedPath.includes("com.termux.documents")) {
-        const dirInfo = options.find(value => value.includes(selectedPath))
-        selectedPath = selectedPath + `::primary:${dirInfo[1]}`;
-      }
-      if (selectedPath.includes("com.termux.documents") && !selectedPath.includes("::/data/data/com.termux/files/home/")) {
-        const dirInfo = options.find(value => value.includes(selectedPath))
-        selectedPath = selectedPath + `::/data/data/com.termux/files/home/${dirInfo[1]}`;
-      }
-
-      return selectedPath;
+    const check = selectedPath.includes("::primary:");
+    if (!check && !selectedPath.includes("com.termux.documents")) {
+      selectedPath = selectedPath + `::primary:${options[1]}`;
+    }
+    if (selectedPath.includes("com.termux.documents") && !selectedPath.includes("::/data/data/com.termux/files/home/")) {
+      selectedPath = selectedPath + `::/data/data/com.termux/files/home/${options[1]}`;
     }
 
-    async cloneRepo() {
+    return selectedPath;
+  }
+
+  async cloneRepo() {
+    fs = new LightningFS("fs", {
+      wipe: true,
+      fileDbName: "rootGitDir"
+    });
+    this.getURL()
+    .then(url => {
+      this.clone(url)
+      .then(dir => {
+        this.run();
+        window.toast("succesfully cloned", 400)
+      })
+      .catch(error => {
+        cAlert(error)})
       fs = new LightningFS("fs", {
         wipe: true,
         fileDbName: "rootGitDir"
       });
+    }).catch(error => cAlert(error))
+  }
 
-      this.getURL()
-      .then(url => {
-        this.clone(url)
-        .then(dir => {
-          this.run();
-          window.toast("succesfully cloned", 400)
-        })
-        .catch(error => {
-          cAlert(error)})
-        fs = new LightningFS("fs", {
-          wipe: true,
-          fileDbName: "rootGitDir"
+  getDirName(url) {
+    const pathArry = new URL(url).pathname.split("/");
+    return pathArry.slice(-1).toString().replace(".git", "");
+  }
+
+  async clone(remotURL) {
+    try {
+
+      dir = "/" + this.getDirName(remotURL);
+      await pfs.mkdir(dir);
+
+      if (remotURL) {
+
+        const cloneload = loader.create('CLONE REPOSITORY', "Cloning Remote", {
+          timeout: 5000,
+          callback: () => window.toast('Loading cancelled', 4000),
         });
-      }).catch(error => cAlert(error))
-    }
-
-    getDirName(url) {
-      const pathArry = new URL(url).pathname.split("/");
-      return pathArry.slice(-1).toString().replace(".git", "");
-    }
-
-    async clone(remotURL) {
-      try {
-        dir = "/" + this.getDirName(remotURL);
-        await pfs.mkdir(dir);
-
-        if (remotURL) {
-          loader.create("Loading", "Fetching data...");
-          const corsProxy = "https://cors.isomorphic-git.org";
-
-          return new Promise((resolve, reject) => {
-            git.clone({
-              fs,
-              http,
-              dir,
-              url: remotURL,
-              corsProxy,
-              onProgress(evt) {
-                loader.create("Loading", "Fetching data...");
-              },
-              onMessage(msg) {
-                loader.create("Loading", msg);
-              },
-              onAuth: (url) => {
-                return this.fillCredentials(url);
-              },
-              onAuthFailure: ({
+        const corsProxy = "https://cors.isomorphic-git.org";
+        return new Promise((resolve, reject) => {
+          loader.show();
+          git.clone({
+            fs,
+            http,
+            dir,
+            url: remotURL,
+            corsProxy,
+            onProgress: (evt)=> {
+              cloneload.setMessage(evt.phase)},
+            onMessage: (msg) => {
+              cloneload.setMessage(msg)
+            },
+            onAuth: (url) => {
+              return this.fillCredentials(url);
+            },
+            onAuthFailure: ({
+              url, auth
+            })=> {
+              return this.rejected({
                 url, auth
-              })=> {
-                return this.rejected({
-                  url, auth
-                });
-              }
-            }).then(() => {
-              window.toast("Successfully Cloned", 4000);
-              loader.destroy();
-              resolve(dir);
-            }).catch((error) => {
-              loader.destroy();
-              reject(error);
-
-            });
+              });
+            }
+          }).then(() => {
+            window.toast("Successfully Cloned", 4000);
+            cloneload.destroy();
+            resolve(dir);
+          }).catch((error) => {
+            cloneload.destroy();
+            reject(error);
           });
-        }
-      } catch (e) {
-        loader.destroy();
-        cAlert(e);
-      }
-    }
-
-    async fillCredentials(url) {
-
-      const multiPrompt = acode.require('multiPrompt');
-
-      const gitCridentailProm = await multiPrompt(
-        'Enter You Cridential',
-        [ {
-          type: 'text',
-          id: 'username',
-          required: true,
-          placeholder: "Username"
-        }, {
-          type: 'password',
-          id: 'password',
-          required: true,
-          placeholder: "Token"
-        },],
-        url,
-      )
-
-      let username = gitCridentailProm["username"]
-
-      let password = gitCridentailProm["password"];
-
-      return {
-        username,
-        password
-      }
-    }
-    async rejected({
-      url, auth
-    }) {
-      cAlert("Authentication rejected");
-      return;
-    }
-
-    createFolderInDevice(structure,
-      location) {
-      for (let key in structure) {
-        if (typeof structure[key] === "object") {
-          // Create a new folder
-          let innerFiles = Object.keys(structure[key]).filter(
-            k => typeof structure[key][k] !== "object"
-          );
-          let newFolder = this.getFolder(key, location, innerFiles)
-          newFolder.then(name => {
-            // Recursively create nested folders
-            this.createFolderInDevice(
-              structure[key], location+"/"+name
-            );
-          })
-        }
-      }
-    }
-    async getFolder(name, location, innerFiles) {
-      const folder = this.createDir(location, name);
-
-      return folder.then(async result => {
-        const structure = await this.createFileStructure("/");
-
-        const filePromises = innerFiles.map(async file => {
-
-          await structure;
-
-          const filepath = this.getPathToRoot(structure, file);
-
-          return this.getFileContent(this.extractPathAndDir(filepath))
-          .then(fileContent => this.createFile(location + "/" + name+"/", file, fileContent))
-          .catch(error => cAlert(error));
         });
-
-        await Promise.all(filePromises);
-
-        return name;
-      }).catch(error => cAlert(error));
-    }
-
-    extractPathAndDir(path) {
-      return {
-        dir: "/" + path.slice(0, path.indexOf("/")),
-        filepath: path.slice(path.indexOf("/") + 1),
-        fileName: path.slice(path.lastIndexOf("/") + 1)
-      };
-    }
-    getPathToRoot(structure, fileName, currentPath = []) {
-      for (const key in structure) {
-        if (typeof structure[key] === "object") {
-          const newPath = currentPath.concat(key);
-          const result = this.getPathToRoot(structure[key], fileName, newPath);
-          if (result) {
-            return result;
-          }
-        } else if (structure[key] === "file" && key === fileName) {
-          currentPath.push(fileName);
-          return currentPath.join("/");
-        }
       }
-      return null;
+    } catch (e) {
+      loader.destroy();
+      cAlert(e);
     }
+  }
 
-    getFileContent( {
-      dir, filepath, fileName
-    }) {
+  async fillCredentials(url) {
 
+    const multiPrompt = acode.require('multiPrompt');
+
+    const gitCridentailProm = await multiPrompt(
+      'Enter You Cridential',
+      [ {
+        type: 'text',
+        id: 'username',
+        required: true,
+        placeholder: "Username"
+      }, {
+        type: 'password',
+        id: 'password',
+        required: true,
+        placeholder: "Token"
+      },],
+      url,
+    )
+
+    let username = gitCridentailProm["username"]
+
+    let password = gitCridentailProm["password"]
+
+    return {
+      username,
+      password
+    }
+  }
+  async rejected({
+    url, auth
+  }) {
+    cAlert("Authentication rejected");
+    return;
+  }
+
+  createFolderInDevice(structure,
+    location) {
+    for (let key in structure) {
+      if (typeof structure[key] === "object") {
+        // Create a new folder
+        let innerFiles = Object.keys(structure[key]).filter(
+          k => typeof structure[key][k] !== "object"
+        );
+        let newFolder = this.getFolder(key, location, innerFiles)
+        newFolder.then(name => {
+          // Recursively create nested folders
+          this.createFolderInDevice(
+            structure[key], location+"/"+name
+          );
+        })
+      }
+    }
+  }
+  async getFolder(name, location, innerFiles) {
+    const folder = this.createDir(location, name);
+
+    return folder.then(async result => {
+      const structure = await this.createFileStructure("/");
+
+      const filePromises = innerFiles.map(async file => {
+
+        await structure;
+
+        const filepath = this.getPathToRoot(structure, file);
+
+        return this.getFileContent(this.extractPathAndDir(filepath))
+        .then(fileContent => this.createFile(location + "/" + name+"/", file, fileContent))
+        .catch(error => cAlert(error));
+      });
+
+      await Promise.all(filePromises);
+
+      return name;
+    }).catch(error => cAlert(error));
+  }
+
+  extractPathAndDir(path) {
+    return {
+      dir: "/" + path.slice(0, path.indexOf("/")),
+      filepath: path.slice(path.indexOf("/") + 1),
+      fileName: path.slice(path.lastIndexOf("/") + 1)
+    };
+  }
+  getPathToRoot(structure, fileName, currentPath = []) {
+    for (const key in structure) {
+      if (typeof structure[key] === "object") {
+        const newPath = currentPath.concat(key);
+        const result = this.getPathToRoot(structure[key], fileName, newPath);
+        if (result) {
+          return result;
+        }
+      } else if (structure[key] === "file" && key === fileName) {
+        currentPath.push(fileName);
+        return currentPath.join("/");
+      }
+    }
+    return null;
+  }
+
+  getFileContent( {
+    dir, filepath, fileName
+  }) {
+
+    return git
+    .resolveRef({
+      fs,
+      dir,
+      ref: "main"
+    })
+    .then(commitOid => {
       return git
-      .resolveRef({
+      .readBlob({
         fs,
         dir,
-        ref: "main"
+        oid: commitOid,
+        filepath // You can replace with 'filepath' if needed
       })
-      .then(commitOid => {
-        return git
-        .readBlob({
-          fs,
-          dir,
-          oid: commitOid,
-          filepath // You can replace with 'filepath' if needed
-        })
-        .then(fileContent => {
-          return new TextDecoder("utf-8").decode(fileContent.blob);
-        })
-        .catch(error => {
-          throw error; // Handle readBlob error
-        });
+      .then(fileContent => {
+        return new TextDecoder("utf-8").decode(fileContent.blob);
       })
       .catch(error => {
-        throw error; // Handle resolveRef error
+        throw error; // Handle readBlob error
       });
-    }
-
-    async createFileStructure(dir) {
-      const {
-        entries
-      } = Object;
-
-      let result = {};
-      const files = await pfs.readdir(dir);
-      for (let file of files) {
-        const filePath = `${dir}/${file}`;
-        const stats = await pfs.stat(filePath);
-
-        if (file === '.git') {
-          continue; // Skip the ".git" directory
-        }
-
-        if (stats.isDirectory()) {
-          result[file] = await this.createFileStructure(filePath);
-        } else {
-          result[file] = "file";
-        }
-      }
-      return Object.fromEntries(entries(result).sort());
-    }
-
-    async deleteDirectory(directoryPath) {
-      try {
-        await fs.promises.rmdir(directoryPath, {
-          recursive: true
-        }); cAlert('Directory deleted successfully.');
-      } catch (error) {
-        cAlert(`Error deleting directory:${error}`);
-      }
-    }
-
-    async getURL() {
-      const options = {
-        required: true,
-        placeholder: "URL (start with https://)",
-        test: value => (value.startsWith("https://") && value.endsWith(".git"))
-      };;
-
-      return prompt(
-        'Enter Remote Url',
-        '',
-        'url',
-        options)
-      .then(url => {
-        if (!url) throw "please provide url"
-        return url
-      })
-      .catch(error => {
-        throw error
-      })
-    }
-
-    async destroy() {
-      editorManager.editor.commands.removeCommand("clone-Repo")
-      let command = {
-        name: "clone-Repository",
-        discription: "clone repository",
-        exec: this.cloneRepo.bind(this)
-      };
-      editorManager.editor.commands.removeCommand(command)
-    }
-  }
-
-
-
-  if (window.acode) {
-    const acodePlugin = new AcodePlugin();
-    acode.setPluginInit(plugin.id, async (baseUrl, $page, {
-      cacheFileUrl, cacheFile
-    }) => {
-      if (!baseUrl.endsWith('/')) {
-        baseUrl += '/';
-      }
-      acodePlugin.baseUrl = baseUrl;
-      await acodePlugin.init($page, cacheFile, cacheFileUrl);
+    })
+    .catch(error => {
+      throw error; // Handle resolveRef error
     });
-    acode.setPluginUnmount(plugin.id,
-      () => {
-        acodePlugin.destroy();
-      });
   }
+
+  async createFileStructure(dir) {
+    const {
+      entries
+    } = Object;
+
+    let result = {};
+    const files = await pfs.readdir(dir);
+    for (let file of files) {
+      const filePath = `${dir}/${file}`;
+      const stats = await pfs.stat(filePath);
+
+      if (file === '.git') {
+        continue; // Skip the ".git" directory
+      }
+
+      if (stats.isDirectory()) {
+        result[file] = await this.createFileStructure(filePath);
+      } else {
+        result[file] = "file";
+      }
+    }
+    return Object.fromEntries(entries(result).sort());
+  }
+
+  async deleteDirectory(directoryPath) {
+    try {
+      await fs.promises.rmdir(directoryPath, {
+        recursive: true
+      }); cAlert('Directory deleted successfully.');
+    } catch (error) {
+      cAlert(`Error deleting directory:${error}`);
+    }
+  }
+
+  async getURL() {
+    const options = {
+      required: true,
+      placeholder: "URL (start with https://)",
+      test: value => (value.startsWith("https://") && value.endsWith(".git"))
+    };;
+
+    return prompt(
+      'Enter Remote Url',
+      '',
+      'url',
+      options)
+    .then(url => {
+      if (!url) throw "please provide url"
+      return url
+    })
+    .catch(error => {
+      throw error
+    })
+  }
+
+  async destroy() {
+    editorManager.editor.commands.removeCommand("clone-Repo")
+    let command = {
+      name: "clone-Repository",
+      discription: "clone repository",
+      exec: this.cloneRepo.bind(this)
+    };
+    editorManager.editor.commands.removeCommand(command)
+  }
+}
+
+
+
+if (window.acode) {
+  const acodePlugin = new AcodePlugin();
+  acode.setPluginInit(plugin.id, async (baseUrl, $page, {
+    cacheFileUrl, cacheFile
+  }) => {
+    if (!baseUrl.endsWith('/')) {
+      baseUrl += '/';
+    }
+    acodePlugin.baseUrl = baseUrl;
+    await acodePlugin.init($page, cacheFile, cacheFileUrl);
+  });
+  acode.setPluginUnmount(plugin.id,
+    () => {
+      acodePlugin.destroy();
+    });
+}
